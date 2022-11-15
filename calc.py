4f22af75d556fdef9b838e91e33908d88cbea539
Nathaniel Todd-Long <ntoddlong@gmail.com>
2022-11-15 06:15:08 +0000

    Key command ctrl-enter works and other stuff added

--- calc.py
+++ calc.py
@@ -28,10 +28,51 @@ data_sizes = [
         "yottabytes",
         ]
 
-def helloCallBack():
-   print()
+def handle_keypress(event):
+    go_callback()
+
+def go_callback():
+    output_box.delete("0.0", tk.END)
+    output_box.insert("0.0", convert(input_box.get("0.0", tk.END)))
+
+def convert(inp) -> str:
+    match input_type.get():
+        case "binary":
+            print("input is binary")
+        case "hex":
+            print("input is hex")
+        case "decimal":
+            print("input is decimal")
+        case "ascii":
+            print("input is ascii")
+        case _:
+            print("input is ",input_type.get())
+    match output_type.get():
+        case "binary":
+            print("output is binary")
+        case "hex":
+            print("output is hex")
+        case "decimal":
+            print("output is decimal")
+        case "ascii":
+            print("output is ascii")
+        case _:
+            print("output is ",output_type.get())
+    return inp
+
+def input_dropdown_callback(selection):
+    pass 
+def output_dropdown_callback(selection):
+    pass
 
 window = tk.Tk()
+
+# bind keypress event to handle_keypress()
+window.bind("<Control-Return>", handle_keypress)
+
+# font
+def_font = tk_font.nametofont("TkDefaultFont")
+def_font.config(size=16)
 
 # dropdown frames
 dropdown_frame = tk.Frame(master=window, bg="#4d4d4d")
@@ -43,10 +84,9 @@ go_frame.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
 output_dropdown_frame = tk.Frame(master=dropdown_frame, bg="gray")
 output_dropdown_frame.pack(fill=tk.BOTH, side=tk.RIGHT, expand=True)
 
-
-B = tk.Button(go_frame, text ="go", command = helloCallBack)
-
-B.pack()
+# go button
+go_button = tk.Button(go_frame, text ="go", command = go_callback)
+go_button.pack()
 
 # input frames
 input_frame = tk.Frame(master=window, bg="gray")
@@ -61,11 +101,11 @@ output_type = tk.StringVar(output_frame)
 output_type.set(data_types[0])
 
 # input data_type dropdown
-input_type_dropdown = tk.OptionMenu(input_dropdown_frame, input_type, *data_types)
+input_type_dropdown = tk.OptionMenu(input_dropdown_frame, input_type, *data_types, command=input_dropdown_callback)
 input_type_dropdown.pack()
 
 # output data_type dropdown
-output_type_dropdown = tk.OptionMenu(output_dropdown_frame, output_type, *data_types)
+output_type_dropdown = tk.OptionMenu(output_dropdown_frame, output_type, *data_types, command=output_dropdown_callback)
 output_type_dropdown.pack()
 
 # input text box
@@ -76,10 +116,4 @@ input_box.pack()
 output_box = tk.Text(master=output_frame)
 output_box.pack()
 
-
-# font
-def_font = tk_font.nametofont("TkDefaultFont")
-def_font.config(size=16)
-
 window.mainloop()
-
